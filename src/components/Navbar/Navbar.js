a3b890cd2bdf086e2be9f04c976fa215cc5fb21b
Harshwardhan <harshwardhansingh21@gmail.com>
2023-10-27 10:31:52 +0530

    temporary changes

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./navbar.css";
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <a href="/Home" className="selectedNavbarHeaderText">
+          Home
+        </a>
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+        <a href="/About" className="navbarHeaderText">
+          About
+        </a>
+      </nav>
+    </header>
+  );
+}
---
+++ src/screen/MainScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import About from "../components/About/About";
+export default function Navbar() {
+  return (
+    <main>
+      <About />
+    </main>
+  );
+}
